833ead91f114af1f11707052a98f8ed0004f7484
Kira Krasnova <vaokyn@gmail.com>
2018-01-17 19:57:37 +0200

    Testing database v7

--- karmator.py
+++ karmator.py
@@ -73,5 +73,5 @@ def webhook_del(password):
 	else:
 		return "Invalid password", 200
 
-print(os.environ.get('PORT', 5000), type(os.environ.get('PORT', 5000)))
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+# print(os.environ.get('PORT', 5000), type(os.environ.get('PORT', 5000)))
+server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
