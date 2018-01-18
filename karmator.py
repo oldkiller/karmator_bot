997c1478dc5f6521598e218401edd4468bc96edd
Kira Krasnova <vaokyn@gmail.com>
2018-01-18 16:20:42 +0200

    Add 'top best' fix v2

--- karmator.py
+++ karmator.py
@@ -83,7 +83,7 @@ def topbest(message):
 	top_mess="Топ благодаримых:\n"
 	for i in range(len(user)):
 		name=user[i][2].strip() if user[i][2].strip() else user[i][3].strip()
-		top_mess+=f"*{i+1}*. {name}\n, ({user[i][1]} раз)"
+		top_mess+=f"*{i+1}*. {name}, ({user[i][1]} раз)\n"
 	bot.send_message(message.chat.id, top_mess, parse_mode="Markdown")
 
 @bot.message_handler(func=lambda message: True if message.reply_to_message else False)
